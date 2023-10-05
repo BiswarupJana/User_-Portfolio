8dd21af078353064d4356da6a4d00b08ed6c64f3
Biswarup Jana <biswaruprx21@gmail.com>
2023-10-05 16:15:59 +0530

    inits

--- server/app.js
+++ server/app.js
@@ -7,11 +7,17 @@ const userRouter = require("./routes/userRouter")
 
 const app = express();
 // 1) Middleware
-app.use(cors({
-  origin: 'https://651e8e266a01f407796292c1--fabulous-sable-6e5530.netlify.app/', // Set the origin to allow requests from
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
-  credentials: true, // Allow cookies and HTTP authentication to be included
-}));
+// app.use(cors({
+//   origin: 'https://651e8e266a01f407796292c1--fabulous-sable-6e5530.netlify.app/', // Set the origin to allow requests from
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
+//   credentials: true, // Allow cookies and HTTP authentication to be included
+// }));
+app.UseCors(x => x
+  .AllowAnyMethod()
+  .AllowAnyHeader()
+  .SetIsOriginAllowed(origin => true) // allow any origin
+  .AllowCredentials()); // allow credentials
+
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
