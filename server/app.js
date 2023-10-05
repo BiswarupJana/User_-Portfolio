96f17348f5c98b16710d47cb8d0e22e13700c261
Biswarup Jana <biswaruprx21@gmail.com>
2023-10-05 16:09:24 +0530

    init

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const userRouter = require("./routes/userRouter")
 const app = express();
 // 1) Middleware
 app.use(cors({
-  origin: 'https://portfolio-kxz5.onrender.com/api/v1/user', // Set the origin to allow requests from
+  origin: 'https://651e8e266a01f407796292c1--fabulous-sable-6e5530.netlify.app/', // Set the origin to allow requests from
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
   credentials: true, // Allow cookies and HTTP authentication to be included
 }));
@@ -23,7 +23,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
 
 
 
